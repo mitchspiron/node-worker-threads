2cc2238cf9bd02191702d9ddadb17ba45f169169
mitchspiron <mitchspiron280@gmail.com>
2025-10-29 08:52:51 +0300

    feat: implement basic server with non-blocking and blocking routes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # node-worker-threads
 Implement Multi-Threading in Node.js With Worker Threads
+
+# Routes
+/non-blocking route: Sends a quick response immediately.
+
+/blocking route: Runs a Worker thread to handle heavy computation.
---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Non-blocking route
+app.get("/non-blocking", (req, res) => {
+  res.status(200).send("This page is non-blocking!");
+});
+
+// Blocking route using worker threads
+app.get("/blocking", (req, res) => {
+  let result = 0;
+  // Simulate heavy computation
+  for (let i = 0; i < 1000000000; i++) {
+    result++;
+  }
+
+  res.status(200).send(`Result is ${result}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
